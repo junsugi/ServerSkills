9be7433bc7248fb4c541c227d9747f9728b2466c
junsugi <wnstjrl96@naver.com>
2026-05-03 22:20:00 +0900

    Listener 생성 #1

---
+++ Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using ServerCore;
+
+namespace ServerSkills;
+
+class Program
+{
+    public static Listener listner;
+    static void Main(string[] args)
+    {
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 5555);
+        
+        listner = new Listener();
+        listner.Init(endPoint, () => new Session());
+    }
+}
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore;
+
+public class Listener
+{
+    private Socket _listenSocket;
+    private Func<Session> _sessionFactory;
+    
+    // Init
+    public void Init(IPEndPoint endPoint, Func<Session> sessionFactory)
+    {
+        _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        _listenSocket.Bind(endPoint);
+        _listenSocket.Listen(10);
+        _sessionFactory = sessionFactory;
+
+        SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+        args.Completed += OnCompletedAccept;
+
+        RegisterAccept(args);
+    }
+    
+    private void RegisterAccept(SocketAsyncEventArgs args)
+    {
+        args.AcceptSocket = null;
+        
+        bool pending = _listenSocket.AcceptAsync(args);
+        if (!pending)
+            OnCompletedAccept(null, args);
+    }
+    
+    private void OnCompletedAccept(object? sender, SocketAsyncEventArgs args)
+    {
+        if (args.SocketError == SocketError.Success)
+        {
+            Session clientSession = _sessionFactory.Invoke();
+            clientSession.Start(args.AcceptSocket);
+            clientSession.OnConnected();
+        }
+        
+        RegisterAccept(args);
+    }
+}

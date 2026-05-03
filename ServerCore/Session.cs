29ad45f840a445576c3c7ca067529106be2c07a7
Junseok Lee <wnstjrl96@naver.com>
2026-05-03 22:21:14 +0900

    Feature/listener (#5)
    * Listener 생성 #1
    
    * 컴파일 오류를 위한 껍데기 클래스 생성 #1
    
    * 설정 파일 #1

---
+++ ServerCore/Session.cs
@@ -0,0 +1,16 @@
+using System.Net.Sockets;
+
+namespace ServerCore;
+
+public class Session
+{
+    public void Start(Socket? argsAcceptSocket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnected()
+    {
+        throw new NotImplementedException();
+    }
+}

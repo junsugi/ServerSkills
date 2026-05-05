a3a30fcba7278f2e3fd382e822a1c7eb957375c7
junsugi <wnstjrl96@naver.com>
2026-05-05 10:42:01 +0900

    패킷 핸들러 추가 #4

---
+++ Server/Packet/PacketManager.cs
@@ -0,0 +1,48 @@
+using Google.Protobuf;
+
+namespace ServerSkills.Packet;
+
+public class PacketManager
+{
+    public static PacketManager Instacne => new PacketManager();
+
+    private Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>> _onRecv =
+        new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>>();
+
+    private Dictionary<ushort, Action<PacketSession, IMessage>> _handler =
+        new Dictionary<ushort, Action<PacketSession, IMessage>>();
+
+    private PacketManager()
+    {
+        Register();
+    }
+
+    private void Register()
+    {
+        // 패킷과 핸들러 등록
+    }
+
+    public void OnRecvPacket(ClientSession session, ArraySegment<byte> buffer)
+    {
+        ushort count = 0;
+
+        ushort dataSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+        count += 2;
+        ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+        count += 2;
+
+        Action<PacketSession, ArraySegment<byte>, ushort> action = null;
+        if (_onRecv.TryGetValue(id, out action))
+            action.Invoke(session, buffer, id);
+    }
+
+    private void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer, ushort id) where T : IMessage, new()
+    {
+        T packet = new T();
+        packet.MergeFrom(buffer.Array, buffer.Offset + 4, buffer.Count - 4);
+
+        Action<PacketSession, IMessage> action = null;
+        if (_handler.TryGetValue(id, out action))
+            action.Invoke(session, packet);
+    }
+}

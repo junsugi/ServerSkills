134a9c6358999dbfb6be2a093c82d4c520ef7121
Junseok Lee <wnstjrl96@naver.com>
2026-05-05 11:29:03 +0900

    Feature/connected (#10)
    * 실제로 웹소켓을 통해 데이터 전송 로직 추가 #9
    
    * 패킷 MsgId에 따라 패킷 만들고 핸들링 하는 부분 추가 #9
    
    * 패킷 MsgId에 따라 패킷 만들고 핸들링 하는 부분 추가 #9
    
    * Protobuf 생성 코드 #9

--- Server/Session/ClientSession.cs
+++ Server/Session/ClientSession.cs
@@ -1,7 +1,23 @@
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+
 namespace ServerSkills;
 
-public class ClientSession : PacketSession
+public partial class ClientSession : PacketSession
 {
+    public void Send(IMessage packet)
+    {
+        string msgName = packet.Descriptor.Name.Replace("_", String.Empty);
+        MsgId msgId = (MsgId)Enum.Parse(typeof(MsgId), msgName);
+        ushort size = (ushort)packet.CalculateSize();
+        byte[] sendBuffer = new byte[size + 4];
+        Array.Copy(BitConverter.GetBytes((ushort)(size + 4)), 0, sendBuffer, 0, sizeof(ushort));
+        Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuffer, 2, sizeof(ushort));
+        Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
+
+        Send(sendBuffer);
+    }
+    
     public override void OnConnected()
     {
         throw new NotImplementedException();

0de8c09d8b90eaec897368defc8027c116909f2e
junsugi <wnstjrl96@naver.com>
2026-05-05 19:12:57 +0900

    DummyClient 생성 로직 #11

---
+++ DummyClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using DummyClient.Session;
+
+namespace DummyClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 5555);
+        
+        Connector connector = new Connector();
+        connector.Connect(endPoint, () =>
+        {
+            DummyClient client = new DummyClient();
+            ServerSession serverSession = new ServerSession();
+
+            client.SetSession(serverSession);
+            serverSession.SetClient(client);
+            
+            return serverSession;
+        }, 10);
+    }
+}

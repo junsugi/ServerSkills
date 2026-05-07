5f64827546bd33da4e1b0e9111a85657155902e7
junsugi <wnstjrl96@naver.com>
2026-05-07 18:09:58 +0900

    모니터링 로직 수정 #21

--- DummyClient/DummyClient.cs
+++ DummyClient/DummyClient.cs
@@ -1,14 +1,16 @@
 using Bogus;
+using DummyClient.Packet;
 using DummyClient.Session;
 using Google.Protobuf.Protocol;
+using ServerSkills.Monitoring;
 using Player = DummyClient.Object.Player;
 
 namespace DummyClient;
 
-public class DummyClient
+public class DummyClient(PendingRequestManager pendingRequestManager)
 {
-    public Player MyPlayer {get; private set;}
-    
+    public Player MyPlayer { get; private set; }
+
     private ServerSession _serverSession;
     private Faker _faker = new Faker();
 
@@ -17,31 +19,47 @@ public class DummyClient
         _serverSession = serverSession;
     }
 
-    public void OnConnected(ResultCode resultCode)
+    public void OnConnected(int requestId, ResultCode resultCode)
     {
+        RequestCompleted(requestId);
+
         if (resultCode != ResultCode.Success)
             return;
 
+        int loginReqId = pendingRequestManager.Register("C_LOGIN", () => { });
         C_Login loginPacket = new C_Login();
         loginPacket.Id = _faker.Name.FullName();
+        loginPacket.RequestId = loginReqId;
         _serverSession.Send(loginPacket);
     }
 
-    public void OnLogin(ResultCode resultCode)
+    public void OnLogin(int requestId, ResultCode resultCode)
     {
+        RequestCompleted(requestId);
+
         if (resultCode != ResultCode.Success)
             return;
-        
+
+        int enterReqId =
+            pendingRequestManager.Register("C_ENTER_GAME", () => { });
         C_EnterGame enterGamePacket = new C_EnterGame();
+        enterGamePacket.RequestId = enterReqId;
         _serverSession.Send(enterGamePacket);
     }
 
-    public void OnEnterGame(ResultCode resultCode, Player? player)
+    public void OnEnterGame(int requestId, ResultCode resultCode, Player? player)
     {
+        RequestCompleted(requestId);
+
         if (resultCode != ResultCode.Success)
             return;
-        
+
         MyPlayer = player!;
-        Console.WriteLine($"{MyPlayer.ObjectType} / {MyPlayer.NickName}");
+        // Console.WriteLine($"{MyPlayer.ObjectType} / {MyPlayer.NickName}");
+    }
+
+    private void RequestCompleted(int requestId)
+    {
+        pendingRequestManager.Completed(requestId);
     }
 }

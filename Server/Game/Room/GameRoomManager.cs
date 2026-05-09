ff4c211208419898ada05292ad16c8e1c3cbf5ca
junsugi <wnstjrl96@naver.com>
2026-05-09 17:18:05 +0900

    GameRoom 생성 #27

---
+++ Server/Game/Room/GameRoomManager.cs
@@ -0,0 +1,47 @@
+namespace ServerSkills.Game.Room;
+
+public class GameRoomManager
+{
+    public static readonly GameRoomManager Instance = new();
+
+    private Dictionary<int, GameRoom> _rooms = new();
+    private int _roomId = 1;
+
+    private GameRoomManager()
+    {
+    }
+
+
+    // 생성
+    public Dictionary<int, GameRoom> Creates(int roomCount = 1)
+    {
+        for (int i = 0; i < roomCount; i++)
+        {
+            _rooms.Add(i, new GameRoom(i));
+        }
+
+        return _rooms;
+    }
+
+    public bool Remove(int roomId)
+    {
+        return _rooms.Remove(roomId);
+    }
+
+    public GameRoom PickRoom(Player player)
+    {
+        int index = Math.Abs(player.ObjectId) % _rooms.Count;
+        return _rooms[index];
+    }
+
+    public void PrintRoomStatus()
+    {
+        foreach (GameRoom room in _rooms.Values.OrderBy(r => r.RoomId))
+        {
+            Console.Write($"[Room {room.RoomId}: Players={room.PlayerCount}] |");
+        }
+
+        Console.WriteLine($"Total Rooms={_rooms.Count}, Total Players={_rooms.Values.Sum(r => r.PlayerCount)}");
+    }
+}
+

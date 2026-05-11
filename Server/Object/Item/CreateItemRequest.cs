3ce916c148b4439f77379cf87023cb5c89222904
junsugi <wnstjrl96@naver.com>
2026-05-11 11:58:16 +0900

    Item 생성 로직 추가 #29

---
+++ Server/Object/Item/CreateItemRequest.cs
@@ -0,0 +1,8 @@
+namespace ServerSkills;
+
+public class CreateItemRequest
+{
+    public int ObjectId { get; set; }
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
---
+++ Server/Object/Item/Item.cs
@@ -0,0 +1,7 @@
+namespace ServerSkills;
+
+public class Item : GameObject
+{
+    public string Name;
+    public int Count;
+}

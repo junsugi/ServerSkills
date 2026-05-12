e9f994dd3d6975f6b49dc8b5b9b4dd4cd3f9ec53
junsugi <wnstjrl96@naver.com>
2026-05-12 16:04:35 +0900

    인벤토리 내부 실패 조건 추가 #31

--- Server/Object/Item/Inventory.cs
+++ Server/Object/Item/Inventory.cs
@@ -6,12 +6,16 @@ public class Inventory
 
     public bool TryAdd(Item item)
     {
+        if (IsFull())
+            return false;
+
+        if (HasItem(item))
+            return false;
+        
         // 테스트 편의를 위해 1종류의 아이템들만 가질 수 있다고 가정.
         return _slots.TryAdd(item.ObjectId, item);
     }
 
-    public bool IsFull()
-    {
-        return _slots.Count == 10;
-    }
+    private bool IsFull() => _slots.Count == 10;
+    private bool HasItem(Item item) => _slots.ContainsKey(item.ObjectId);
 }

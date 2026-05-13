c81d297d0f8967d450a4758e9119dfd928d40582
junsugi <wnstjrl96@naver.com>
2026-05-13 17:54:41 +0900

    Position 추가 #33

--- DummyClient/Object/Position.cs
+++ DummyClient/Object/Position.cs
@@ -4,4 +4,18 @@ public class Position
 {
     public float X { get; set; }
     public float Y { get; set; }
+
+    public Position(float x, float y)
+    {
+        X = x;
+        Y = y;   
+    }    
+    
+    public Google.Protobuf.Protocol.Position ToDto()
+    {
+        Google.Protobuf.Protocol.Position positionInfo = new Google.Protobuf.Protocol.Position();
+        positionInfo.X = X;
+        positionInfo.Y = Y;
+        return positionInfo;
+    }
 }
